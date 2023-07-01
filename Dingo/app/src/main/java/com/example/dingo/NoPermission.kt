200cc272e07c025f1de2df0a5e9583e8f5d54bdd
Philip Chen <philip.chen.0223@gmail.com>
2023-06-30 21:54:00 -0400

    Improved camera permissions UI

--- Dingo/app/src/main/java/com/example/dingo/NoPermission.kt
+++ Dingo/app/src/main/java/com/example/dingo/NoPermission.kt
@@ -7,13 +7,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.rounded.AddCircle
 import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Warning
+import androidx.compose.material3.Icon
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -23,9 +28,25 @@ import androidx.navigation.compose.rememberNavController
 fun NoPermission(
     onRequestPermission: () -> Unit
 ) {
-    NoPermissionContent(
-        onRequestPermission = onRequestPermission
-    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        Icon(
+            imageVector = Icons.Rounded.Warning,
+            contentDescription = "warning_icon"
+        )
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = "Please grant permission to use the Camera",
+            textAlign = TextAlign.Center
+        )
+        Button(onClick = onRequestPermission) {
+            Text("Try Again ")
+        }
+    }
 }
 
 @Composable
